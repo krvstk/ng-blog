2db51acae2b0f2f036a640c66f678ec72a69c77f
dolph <y.blyzniuk@o3.ua>
2020-10-18 17:05:49 +0300

    (new UI item) header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ExtendedModule, FlexModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexModule,
+    MatGridListModule,
+    ExtendedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
